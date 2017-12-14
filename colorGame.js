ea858c486163f7afaafe43ba0df7d70caef613ae
Liang Zhang <zhangliangnuaa@gmail.com>
2017-12-14 17:15:17 +0800

    Randomize picked color, add display message, improve correct and wrong react display

--- colorGame.js
+++ colorGame.js
@@ -8,8 +8,9 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,9 +20,22 @@ for (var i = 0; i < squares.length; i++) {
 	squares[i].addEventListener("click", function () {
 		var clickedColor = this.style.backgroundColor;
 		if (clickedColor === pickedColor) {
-			alert("Correct!");
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
 		} else{
-			alert("Wrong!!!");
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	});
+}
+
+function changeColors(clickedColor) {
+	for (var i = 0; i < squares.length; i++) {
+			squares[i].style.backgroundColor = clickedColor;
+		}	
+}
+
+function pickColor() {
+	var index = Math.floor(Math.random() * colors.length);
+	return colors[index];
 }
